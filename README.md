9baafc5a9573118095b8f192ec1f33dc19a5d690
Jason Rahm <jasonrahm@gmail.com>
2023-05-21 14:36:31 -0600

    vite installed and basic scene created

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# threejs-journey
+# ThreeJS Journey 
+This project includes lesson files from the [ThreeJS Journey](https://threejs-journey.com/) course I purchased to learn how to build 3D websites and web experiences.
--- src/script.js
+++ src/script.js
@@ -1,17 +1,19 @@
-// Sizes
-const sizes = {
-  width: 800,
-  height: 600
-}
+import * as THREE from 'three'
 
 // Scene
 const scene = new THREE.Scene()
 
-// Red cube
+// Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+}
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
@@ -19,9 +21,8 @@ camera.position.z = 3
 scene.add(camera)
 
 // Renderer
-const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
-  canvas
+  canvas: document.querySelector('canvas.webgl'),
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
