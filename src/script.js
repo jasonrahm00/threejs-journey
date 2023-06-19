fbab10eb523ccdb82dea8b98d46af3fa5b138274
Jason Rahm <jasonrahm@gmail.com>
2023-06-19 17:41:02 -0600

    orbit controls added

--- src/script.js
+++ src/script.js
@@ -1,13 +1,16 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Sizes
 const sizes = {
   width: 800,
   height: 600,
 }
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
@@ -20,17 +23,18 @@ const mesh = new THREE.Mesh(
 scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 100)
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -39,13 +43,9 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Animate
-const clock = new THREE.Clock()
-
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
-
-  // Update objects
-  mesh.rotation.y = elapsedTime
+  // Update controls
+  controls.update()
 
   // Render
   renderer.render(scene, camera)
