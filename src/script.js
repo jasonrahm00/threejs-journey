7d54c09c5134cca01cae992f882a365e23cc67a5
Jason Rahm <jasonrahm@gmail.com>
2023-06-19 16:45:24 -0600

    near and far values added to perspective camera

--- src/script.js
+++ src/script.js
@@ -1,17 +1,7 @@
 import * as THREE from 'three'
-import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -19,9 +9,27 @@ const sizes = {
   height: 600,
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -29,25 +37,21 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
-// Clock
-gsap.to(mesh.position, {
-  duration: 1,
-  delay: 1,
-  x: 2,
-})
+// Animate
+const clock = new THREE.Clock()
 
-gsap.to(mesh.position, {
-  duration: 1,
-  delay: 2,
-  x: 0,
-})
-
-// Animations
 const tick = () => {
-  // // Render
+  const elapsedTime = clock.getElapsedTime()
+
+  // Update objects
+  mesh.rotation.y = elapsedTime
+
+  // Render
   renderer.render(scene, camera)
+
+  // Call tick again on the next frame
   window.requestAnimationFrame(tick)
 }
+
 tick()
