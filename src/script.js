291b7a119c946942aff53b136d8fa16d1b1e9121
Jason Rahm <jasonrahm@gmail.com>
2023-06-19 16:09:45 -0600

    simple box animation added

--- src/script.js
+++ src/script.js
@@ -1,38 +1,17 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
 // Object
-const group = new THREE.Group()
-group.position.y = 1
-group.scale.y = 2
-group.rotation.y = 1
-scene.add(group)
-
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-group.add(cube1)
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-)
-cube2.position.x = -2
-group.add(cube2)
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff })
-)
-cube3.position.x = 2
-group.add(cube3)
-
-// Axes Helper
-const axesHelper = new THREE.AxesHelper()
-scene.add(axesHelper)
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -47,7 +26,28 @@ scene.add(camera)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('canvas.webgl'),
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+// Clock
+gsap.to(mesh.position, {
+  duration: 1,
+  delay: 1,
+  x: 2,
+})
+
+gsap.to(mesh.position, {
+  duration: 1,
+  delay: 2,
+  x: 0,
+})
+
+// Animations
+const tick = () => {
+  // // Render
+  renderer.render(scene, camera)
+  window.requestAnimationFrame(tick)
+}
+tick()
